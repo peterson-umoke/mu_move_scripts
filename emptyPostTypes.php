ff74b47c2ef63e563ec3deaf483ad35628dd3618
Peterson Umoke <umoke10@hotmail.com>
2018-10-07 16:07:05 +0100

    use this to empty all posts except for products

--- emptyPostTypes.php
+++ emptyPostTypes.php
@@ -5,3 +5,51 @@ require_once dirname(__FILE__) . "/vendor/autoload.php";
 
 // init the database connection
 $db = new CustomDatabase();
+for ($i = 2; $i <= 14; $i++) {
+
+    // set the prefix
+    $prefix = "wp_{$i}_";
+
+    // make sure we dont upload products in a post
+    if ($i !== 6) {
+        $post_type = "post";
+        // get all related posts
+        $result = $this->initDB()->count("{$prefix}posts", "*", [
+            "post_type" => $post_type
+        ]);
+
+        // loop
+        foreach ($result as $key => $value) {
+            $this->initDB()->delete("{$prefix}postmeta", [
+                "post_id" => $value['ID']
+            ]);
+            echo "done deteting meta" . PHP_EOL;
+        }
+
+        // delete posts
+        $this->initDB()->delete("{$prefix}posts", [
+            "post_type" => $post_type
+        ]);
+        echo "deleted all post types for {$post_type}" . PHP_EOL;
+    } else {
+        $post_type = "product";
+        // get all related posts
+        $result = $this->initDB()->count("{$prefix}posts", "*", [
+            "post_type" => $post_type
+        ]);
+
+        // loop
+        foreach ($result as $key => $value) {
+            $this->initDB()->delete("{$prefix}postmeta", [
+                "post_id" => $value['ID']
+            ]);
+            echo "done deteting meta" . PHP_EOL;
+        }
+
+        // delete posts
+        $this->initDB()->delete("{$prefix}posts", [
+            "post_type" => $post_type
+        ]);
+        echo "deleted all post types for {$post_type}" . PHP_EOL;
+    }
+}
