625c49f4297aebdee02d7e92e9099cf07c828249
Peterson Umoke <umoke10@hotmail.com>
2018-10-07 19:03:23 +0100

    just corrected the sql for the deletion process

--- deleteProductTypes.php
+++ deleteProductTypes.php
@@ -16,22 +16,7 @@ WHERE
     term_taxonomy.taxonomy = 'product_type'
 AND terms.slug = 'gidi_hotel_rooms'
 AND posts.post_type = 'product'";
-$sql = "SELECT
-    *
-FROM
-    {$prefix}posts AS posts
-INNER JOIN {$prefix}term_relationships AS term_relationships ON posts.ID = term_relationships.object_id
-INNER JOIN {$prefix}term_taxonomy AS term_taxonomy ON term_relationships.term_taxonomy_id = term_taxonomy.term_taxonomy_id
-INNER JOIN {$prefix}terms AS terms ON term_taxonomy.term_id = terms.term_id
-WHERE
-    term_taxonomy.taxonomy = 'product_type'
-AND terms.slug = '{$product_type}'
-AND posts.post_type = 'product'";
 $db = new CustomDatabase();
-$res = $db->query($sql)->fetchAll();
-
-print_r($res);
-
-// foreach ($res as $key => $val) {
-//     // $db->delete()
-// }
+if ($db->query($sql)->fetchAll()) {
+    echo "done delete post types";
+}
